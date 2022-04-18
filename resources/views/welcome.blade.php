7a2e573c9e9fe0c84abb7269d136398781546bc1
Caen De Silva <caen@desilva.se>
2022-04-18 19:31:29 +0200

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Try HydePHP!</title>
+</head>
+
+<body>
+    <header class="mx">
+    </header>
+    <div class="p-5 bg-light">
+        <div class="container">
+            <h1>Try HydePHP!</h1>
+            <p class="lead">Try HydePHP right in your browser!</p>
+            <hr class="my-2">
+            <p>More info on <a href="https://hydephp.github.io/">https://hydephp.github.io/</a></p>
+            <p class="lead">
+                <a class="btn btn-primary" href="https://hydephp.github.io/" role="button">Install Hyde!</a>
+            </p>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
