5c1801337c0409204dd91a1b9d2c6332dc76414f
Caen De Silva <caen@desilva.se>
2022-04-18 21:45:02 +0200

    Add download and preview route

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/preview', function () {
+    $markdown = file_get_contents(resource_path('markdown/example.md'));
+    $document = Spatie\YamlFrontMatter\YamlFrontMatter::markdownCompatibleParse($markdown);
+    $post = new Hyde\Framework\Models\MarkdownPost($document->matter(), $document->body());
+
+    return view('hyde::layouts/post')->with([
+        'post' => $post,
+        'title' => $post->title ?? 'My New Post',
+        'markdown' => Hyde\Framework\Actions\MarkdownConverter::parse($post->body),
+        'currentPage' => 'posts/demo',
+    ])->render();
+});
+
+
 Route::post('/api/hyde/post/store', [HydePostController::class, 'store'])->name('hyde.post.store');
+Route::get('/api/hyde/post/{post}/download', [HydePostController::class, 'download'])->name('hyde.post.download');
 Route::get('/hyde/post/render/{post}', [HydePostController::class, 'render'])->name('hyde.post.render');
